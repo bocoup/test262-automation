743685aa738184606f885daa7917353f046ab3ef
bmac <bmac325@gmail.com>
2018-06-19 15:27:58 -0400

    use real default params

--- utils/pr.js
+++ utils/pr.js
@@ -11,8 +11,7 @@ const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
    Opens a pull request on the test262 repo. Requires the
    `GITHUB_TOKEN` enviroment variable to be set.
 */
-function openPullRequest(branchName, title, body, _axios) {
-  _axios = _axios || axios
+function openPullRequest(branchName, title, body, axios=axios) {
   assert(GITHUB_TOKEN, 'No github token found. Please set the GITHUB_TOKEN enviroment variable before attempting to open a pull request.');
   let headers = {
     Authorization: `token ${GITHUB_TOKEN}`,
@@ -20,7 +19,8 @@ function openPullRequest(branchName, title, body, _axios) {
   };
 
   // POST /repos/:owner/:repo/pulls
-  return _axios.post(`https://api.github.com/repos/${T262_GH_ORG}/${T262_GH_REPO_NAME}/pulls`, {
+  // https://developer.github.com/v3/pulls/#create-a-pull-request
+  return axios.post(`https://api.github.com/repos/${T262_GH_ORG}/${T262_GH_REPO_NAME}/pulls`, {
     title,
     body,
     head: `${GITHUB_USERNAME}:${branchName}`,
