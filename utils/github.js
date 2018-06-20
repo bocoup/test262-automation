52dd404005c3e6f5520a5d6f32901bf3c5febf39
bmac <bmac325@gmail.com>
2018-06-20 11:43:24 -0400

    Update github class based on feedback from rick

--- utils/github.js
+++ utils/github.js
@@ -1,4 +1,4 @@
-const fetch = require('isomorphic-fetch');
+const isomorphicFetch = require('isomorphic-fetch');
 const assert = require('assert');
 
 const T262_GH_ORG = process.env.T262_GH_ORG || 'tc39';
@@ -7,15 +7,14 @@ const T262_BASE_BRANCH = process.env.T262_BASE_BRANCH || 'master';
 const GITHUB_USERNAME = process.env.GITHUB_USERNAME || 'test262-automation';
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
-const FETCH_SYM = Symbol('fetch')
-const TOKEN_SYM = Symbol('token')
+const FETCH_SYM = Symbol('fetch');
+const TOKEN_SYM = Symbol('token');
 
 class GitHub {
 
-  constructor(githubToken=GITHUB_TOKEN, _fetch=fetch) {
-    // use a symbol to make rick happy
-    this[FETCH_SYM] = _fetch
-    this[TOKEN_SYM] = githubToken
+  constructor(githubToken=GITHUB_TOKEN, fetch=isomorphicFetch) {
+    this[FETCH_SYM] = fetch;
+    this[TOKEN_SYM] = githubToken;
     assert(this[TOKEN_SYM], 'No github token found. Please set the GITHUB_TOKEN enviroment variable before attempting to open a pull request.');
   }
 
@@ -26,15 +25,8 @@ class GitHub {
      https://developer.github.com/v3/pulls/#create-a-pull-request
   */
   openPullRequest(branchName, title, body) {
-    let headers = {
-      Authorization: `token ${GITHUB_TOKEN}`,
-      Accept: 'application/vnd.github.v3+json',
-      'content-type': 'application/json'
-    };
-
     return this.request({
       path: `/repos/${T262_GH_ORG}/${T262_GH_REPO_NAME}/pulls`,
-      method: 'POST',
       body: {
         title,
         body,
@@ -42,29 +34,24 @@ class GitHub {
         base: T262_BASE_BRANCH,
         maintainer_can_modify: true,
       }
-    })
+    });
   }
 
   /**
      Adds a label to a GitHub pr (or issue).
 
+     POST /repos/:owner/:repo/issues/:number/labels
      https://developer.github.com/v3/issues/labels/#add-labels-to-an-issue
   */
-  addLabel(pr_number, label) {
-    let headers = {
-      Authorization: `token ${GITHUB_TOKEN}`,
-      Accept: 'application/vnd.github.v3+json',
-      'content-type': 'application/json'
-    };
-
+  addLabel(number, label) {
     return this.request({
-      method: 'POST',
-      path: `/repos/${T262_GH_ORG}/${T262_GH_REPO_NAME}/issues/${pr_number}/labels`,
+      path: `/repos/${T262_GH_ORG}/${T262_GH_REPO_NAME}/issues/${number}/labels`,
       body: [label]
-    })
+    });
   }
 
-  async request({method, path, body}) {
+
+  async request({path, body}) {
     let headers = {
       Authorization: `token ${this[TOKEN_SYM]}`,
       Accept: 'application/vnd.github.v3+json',
@@ -75,7 +62,7 @@ class GitHub {
       body: JSON.stringify(body),
       headers,
       method: 'POST'
-    })
+    });
 
     if (response.status === 200) {
       return response.json();
@@ -84,7 +71,7 @@ class GitHub {
       // error messages. Although it might be possible for a 500 error
       // not to return json...
       let json = await response.json();
-      return Promise.reject(json)
+      return Promise.reject(json);
     }
   }
 }
