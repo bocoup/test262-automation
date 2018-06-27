bebf4661cc2ca0539d7868bbf3857767bc84c28d
bmac <bmac325@gmail.com>
2018-06-27 12:43:53 -0400

    Refactor outcomeReporter

--- utils/outcomeReporter.js
+++ utils/outcomeReporter.js
@@ -1,14 +1,4 @@
 const constants = require('./constants')
-const Handlebars = require('handlebars')
-
-// Very dump pluralization helper. In practice we only use this to
-// pluralize the word file/files
-Handlebars.registerHelper('pluralize', function(string, count) {
-  if (count === 1) {
-    return string;
-  }
-  return string + 's';
-});
 
 const {
   DO_NOT_EXPORT,
@@ -27,233 +17,18 @@ const {
   UPDATE_EXTENSION_ON_TARGET_FILE,
 } = constants.fileOutcomes;
 
-var outcomes = { '0':
-   { description: [ 'DO_NOT_EXPORT.....' ],
-     files:
-      [ '/262-partially-curated-vendor-not-modified/empty-function.js',
-        '/262-partially-curated-vendor-rename/function-toString-arrow.js',
-        '/RENAME_MODIFIED_TARGET_FILE_WITH_NOTE_ON_RENAME/import-basic.js',
-        '/UPDATE_EXTENSION_ON_MODIFIED_TARGET_FILE_WITH_NOTE_ON_EXTENSION_CHANGE/inferred-names.js' ] },
-  '1':
-   { description: [ 'DO_NOT_EXPORT_AND_BLOCK_FUTURE_EXPORTS.....' ],
-     files:
-      [ '/262-fully-curated-vendor-not-modified/array-flatten.js' ] },
-  '2':
-   { description: [ 'EXPORT_AND_OVERWRITE_PREVIOUS_VERSION.....' ],
-     files:
-      [ '/262-not-modified-vendor-modified/class-static-get-weird.js' ] },
-  '3':
-   { description: [ 'APPEND_MODIFIED_TARGET_WITH_NOTE_AND_NEW_SOURCE.....' ],
-     files:
-      [ '/262-partially-curated-vendor-modified/duplicate-computed-accessors.js',
-        '/APPEND_MODIFIED_TARGET_WITH_NOTE_AND_NEW_SOURCE/generator-return.js' ] },
-  '4':
-   { description:
-      [ 'RE_EXPORT_SOURCE_WITH_NOTE_ON_PREVIOUS_TARGET_DELETION.....' ],
-     files:
-      [ '/262-fully-curated-vendor-modified/array-indexof.js',
-        '/RE_EXPORT_SOURCE_WITH_NOTE_ON_PREVIOUS_TARGET_DELETION/global-is-nan.js' ] },
-  '5':
-   { description:
-      [ 'RE_EXPORT_RENAMED_SOURCE_WITH_NOTE_ON_PREVIOUS_TARGET_DELETION.....' ],
-     files: [] },
-  '6':
-   { description:
-      [ 'RE_EXPORT_SOURCE_NEW_EXTENSION_WITH_NOTE_ON_PREVIOUS_TARGET_DELETION_AND_EXTENSION.....' ],
-     files: [] },
-  '7':
-   { description: [ 'DELETE_TARGET_FILE.....' ],
-     files:
-      [ '/262-not-modified-vendor-deleted/builtin-function-name.js',
-        '/DELETE_TARGET_FILE/have-a-bad-time-with-arguments.js' ] },
-  '8':
-   { description: [ 'RENAME_TARGET_FILE.....' ],
-     files:
-      [ '/262-fully-curated-vendor-rename/basic-weakmap-post-rename.js,',
-        '/262-not-modified-vendor-rename/custom-iterators-post-rename.js,',
-        '/262-partially-curated-vendor-rename/function-toString-arrow-post-rename.js,',
-        '/RE_EXPORT_RENAMED_SOURCE_WITH_NOTE_ON_PREVIOUS_TARGET_DELETION/generator-yield-star-post-rename.js,',
-        '/RE_EXPORT_SOURCE_NEW_EXTENSION_WITH_NOTE_ON_PREVIOUS_TARGET_DELETION_AND_EXTENSION/has-custom-properties.jsx,',
-        '/UPDATE_EXTENSION_ON_TARGET_FILE/instanceof.jsx,' ] },
-  '9':
-   { description:
-      [ 'APPEND_MODIFIED_TARGET_WITH_NOTE_ON_SOURCE_DELETION.....' ],
-     files:
-      [ '/262-partially-curated-vendor-deleted/date-negative-zero.js',
-        '/APPEND_MODIFIED_TARGET_WITH_NOTE_ON_SOURCE_DELETION/has-own-property-called-on-non-object.js' ] },
-  '10':
-   { description: [ 'RENAME_MODIFIED_TARGET_FILE_WITH_NOTE_ON_RENAME.....' ],
-     files: [] },
-  '11':
-   { description:
-      [ 'UPDATE_EXTENSION_ON_MODIFIED_TARGET_FILE_WITH_NOTE_ON_EXTENSION_CHANGE.....' ],
-     files: [] },
-  '12':
-   { description: [ 'EXPORT_FILE.....' ],
-     files:
-      [ '/262-does-not-exist-vendor-new-exportable-file/for-in-array-mode.js',
-        '/262-does-not-exist-vendor-new-unexportable-file/es6.yaml' ] },
-  '13':
-   { description: [ 'UPDATE_EXTENSION_ON_TARGET_FILE.....' ],
-     files: [] } }
+const Handlebars = require('handlebars')
 
-
-class OutcomeReporter {
-
-  renderHeading({vendor, sourceSha, targetSha}) {
-    return `
-# Import JavaScript Test Changes from ${vendor}
-
-Changes imported in this pull request include all changes made since
-\`${sourceSha}\` in ${vendor} and all changes made since \`${targetSha}\` in
-test262.
-    `
+// Very dumb pluralization helper. In practice we only use this to
+// pluralize the word file/files
+function pluralize(string, count) {
+  if (count === 1) {
+    return string;
   }
-
-  renderFileList(files) {
-    return files.map(file => {
-      return ` - ${file}`
-    }).join('\n');
-  }
-
-  renderSubSection(sectionId, sectionInfo, vendor) {
-    console.log(sectionInfo)
-    var templates = TEMPLATES[sectionId]
-    var subTitle = Handlebars.compile(templates.subTitle)
-    var description = Handlebars.compile(templates.description)
-    var context = {
-      fileCount: sectionInfo.files.length,
-      vendor: vendor,
-      contribDirectory: `implementor-contribDirectory/${vendor}`
-    };
-
-    if (!sectionInfo.files.length) {
-      return '';
-    }
-
-    return `
-### ${subTitle(context)}
-
-${description(context)}
-
-${this.renderFileList(sectionInfo.files)}
-`
-  }
-
-  generateReport({
-    sourceSha,
-    targetSha,
-    vendor,
-    outcomes
-  }) {
-    var sections = Object.entries(outcomes).map(([sectionId, section]) => {
-      return this.renderSubSection(sectionId, section, vendor);
-    });
-    return `
-${this.renderHeading({sourceSha, targetSha, vendor})}
-
-${sections.join('')}
-`
-  }
-
-  generateReport1(outcomes) {
-
-    return `
-      # Import JavaScript Test Changes from JSC
-
-      ### 2 new files added in JSC
-
-      These files were added in JSC and have been synced to the
-      implementor-contributed directory.
-
-      - /262-does-not-exist-vendor-new-exportable-file/for-in-array-mode.js
-      - /262-does-not-exist-vendor-new-unexportable-file/es6.yaml
-
-      ### 0 partially curated files have been renamed to match JSC
-      UPDATE_EXTENSION_ON_MODIFIED_TARGET_FILE_WITH_NOTE_ON_EXTENSION_CHANGE.....
-
-      ### 0 partially curated files have been renamed to match JSC
-
-      These files were partially curated in test262 and have been
-      renamed in JSC. A comment has been added noting the files past
-      history.
-
-
-      ### 2 partially curated files have been deleted in JSC
-
-      A comment has been added to these files noting their deletion in
-      JSC.
-
-      - /262-partially-curated-vendor-deleted/date-negative-zero.js
-      - /APPEND_MODIFIED_TARGET_WITH_NOTE_ON_SOURCE_DELETION/has-own-property-called-on-non-object.js
-
-      ### 6 files have been renamed
-
-      These files were renamed in JSC and have had their filenames
-      updated in implementor-contributed.
-
-      - /262-fully-curated-vendor-rename/basic-weakmap-post-rename.js
-      - /262-not-modified-vendor-rename/custom-iterators-post-rename.js
-      - /262-partially-curated-vendor-rename/function-toString-arrow-post-rename.js
-      - /RE_EXPORT_RENAMED_SOURCE_WITH_NOTE_ON_PREVIOUS_TARGET_DELETION/generator-yield-star-post-rename.js
-      - /RE_EXPORT_SOURCE_NEW_EXTENSION_WITH_NOTE_ON_PREVIOUS_TARGET_DELETION_AND_EXTENSION/has-custom-properties.jsx
-      - /UPDATE_EXTENSION_ON_TARGET_FILE/instanceof.jsx
-
-      ### 2 files have been deleted
-
-      These files have been deleted in JSC and are removed from the
-      implementation-contributed directory.
-
-      - /262-not-modified-vendor-deleted/builtin-function-name.js
-      - /DELETE_TARGET_FILE/have-a-bad-time-with-arguments.js
-
-      ### 0 files that were previously curated have been given a new extension in JSC
-
-      These files have been reintroduced into the
-      implementation-contributed directory with a comment specifying
-      they were previously curated and deleted.
-
-      - 
-
-      ### 0 files that were previously curated have been renamed in JSC
-
-      These files have been reintroduced into the
-      implementation-contributed directory with a comment specifying
-      they were previously curated and deleted.
-
-      - 
-
-      ### 2 files that were previously curated have been updated in JSC
-
-      These files have been reintroduced into the
-      implementation-contributed directory with a comment specifying
-      they were previously curated and deleted.
-
-      - /262-fully-curated-vendor-modified/array-indexof.js
-      - /RE_EXPORT_SOURCE_WITH_NOTE_ON_PREVIOUS_TARGET_DELETION/global-is-nan.js
-
-      ### 2 Files with changed in both test262 and JSC
-      - /262-partially-curated-vendor-modified/duplicate-computed-accessors.js
-      - /APPEND_MODIFIED_TARGET_WITH_NOTE_AND_NEW_SOURCE/generator-return.js
-
-      ### 1 file updated from jsc
-      - /262-not-modified-vendor-modified/class-static-get-weird.js
-
-      ### 1 File classified as fully curated
-      These files will be ignored in future imports
-      - /262-fully-curated-vendor-not-modified/array-flatten.js
-
-      ### 4 Ignored Files
-      - /262-partially-curated-vendor-not-modified/empty-function.js
-      - /262-partially-curated-vendor-rename/function-toString-arrow.js
-      - /RENAME_MODIFIED_TARGET_FILE_WITH_NOTE_ON_RENAME/import-basic.js
-      - /UPDATE_EXTENSION_ON_MODIFIED_TARGET_FILE_WITH_NOTE_ON_EXTENSION_CHANGE/inferred-names.js
-    `
-  }
+  return string + 's';
 }
 
-// add header with shas
-
+Handlebars.registerHelper('pluralize', pluralize);
 
 /*
   Template variables:
@@ -366,16 +141,72 @@ These are new files added in {{ vendor }} and have been synced to the
   },
 }
 
+class OutcomeReporter {
 
+  constructor({ implConfig, githubConfig }) {
+    this.implConfig = implConfig
+    this.githubConfig = githubConfig
+  }
 
-var reporter = new OutcomeReporter()
+  get contribDirectory() {
+    return this.implConfig.targetSubDirectory
+  }
 
-var report = reporter.generateReport({
-  sourceSha: 'abc',
-  targetSha: '123',
-  vendor: 'jsc',
-  outcomes
-})
+  renderHeading({vendor, sourceSha, targetSha}) {
+    return `
+# Import JavaScript Test Changes from ${vendor}
 
+Changes imported in this pull request include all changes made since
+\`${sourceSha}\` in ${vendor} and all changes made since \`${targetSha}\` in
+test262.
+`
+  }
 
-console.log(report)
+  renderFileList(files) {
+    return files.map(file => {
+      return ` - ${this.contribDirectory}${file}`
+    }).join('\n');
+  }
+
+  renderSubSection(sectionId, sectionInfo, vendor) {
+    var templates = TEMPLATES[sectionId]
+    var subTitle = Handlebars.compile(templates.subTitle)
+    var description = Handlebars.compile(templates.description)
+    var context = {
+      fileCount: sectionInfo.files.length,
+      vendor: vendor,
+      contribDirectory: this.contribDirectory,
+    };
+
+    if (!sectionInfo.files.length) {
+      return '';
+    }
+
+    return `
+### ${subTitle(context)}
+
+${description(context)}
+
+${this.renderFileList(sectionInfo.files)}
+`
+  }
+
+  generateReport({
+    sourceSha,
+    targetSha,
+    vendor,
+    outcomes
+  }) {
+    var sections = Object.entries(outcomes).map(([sectionId, section]) => {
+      return this.renderSubSection(sectionId, section, vendor);
+    });
+    return `
+${this.renderHeading({sourceSha, targetSha, vendor})}
+
+${sections.join('')}
+`
+  }
+}
+
+module.exports = OutcomeReporter;
+
---
+++ utils/outcomeReporter.test.js
@@ -0,0 +1,169 @@
+const OutcomeReporter = require('./outcomeReporter')
+
+var outcomes = { '0':
+                 { description: [ 'DO_NOT_EXPORT.....' ],
+                   files:
+                   [ '/262-partially-curated-vendor-not-modified/empty-function.js',
+                     '/262-partially-curated-vendor-rename/function-toString-arrow.js',
+                     '/RENAME_MODIFIED_TARGET_FILE_WITH_NOTE_ON_RENAME/import-basic.js',
+                     '/UPDATE_EXTENSION_ON_MODIFIED_TARGET_FILE_WITH_NOTE_ON_EXTENSION_CHANGE/inferred-names.js' ] },
+                 '1':
+                 { description: [ 'DO_NOT_EXPORT_AND_BLOCK_FUTURE_EXPORTS.....' ],
+                   files:
+                   [ '/262-fully-curated-vendor-not-modified/array-flatten.js' ] },
+                 '2':
+                 { description: [ 'EXPORT_AND_OVERWRITE_PREVIOUS_VERSION.....' ],
+                   files:
+                   [ '/262-not-modified-vendor-modified/class-static-get-weird.js' ] },
+                 '3':
+                 { description: [ 'APPEND_MODIFIED_TARGET_WITH_NOTE_AND_NEW_SOURCE.....' ],
+                   files:
+                   [ '/262-partially-curated-vendor-modified/duplicate-computed-accessors.js',
+                     '/APPEND_MODIFIED_TARGET_WITH_NOTE_AND_NEW_SOURCE/generator-return.js' ] },
+                 '4':
+                 { description:
+                   [ 'RE_EXPORT_SOURCE_WITH_NOTE_ON_PREVIOUS_TARGET_DELETION.....' ],
+                   files:
+                   [ '/262-fully-curated-vendor-modified/array-indexof.js',
+                     '/RE_EXPORT_SOURCE_WITH_NOTE_ON_PREVIOUS_TARGET_DELETION/global-is-nan.js' ] },
+                 '5':
+                 { description:
+                   [ 'RE_EXPORT_RENAMED_SOURCE_WITH_NOTE_ON_PREVIOUS_TARGET_DELETION.....' ],
+                   files: [] },
+                 '6':
+                 { description:
+                   [ 'RE_EXPORT_SOURCE_NEW_EXTENSION_WITH_NOTE_ON_PREVIOUS_TARGET_DELETION_AND_EXTENSION.....' ],
+                   files: [] },
+                 '7':
+                 { description: [ 'DELETE_TARGET_FILE.....' ],
+                   files:
+                   [ '/262-not-modified-vendor-deleted/builtin-function-name.js',
+                     '/DELETE_TARGET_FILE/have-a-bad-time-with-arguments.js' ] },
+                 '8':
+                 { description: [ 'RENAME_TARGET_FILE.....' ],
+                   files:
+                   [ '/262-fully-curated-vendor-rename/basic-weakmap-post-rename.js,',
+                     '/262-not-modified-vendor-rename/custom-iterators-post-rename.js,',
+                     '/262-partially-curated-vendor-rename/function-toString-arrow-post-rename.js,',
+                     '/RE_EXPORT_RENAMED_SOURCE_WITH_NOTE_ON_PREVIOUS_TARGET_DELETION/generator-yield-star-post-rename.js,',
+                     '/RE_EXPORT_SOURCE_NEW_EXTENSION_WITH_NOTE_ON_PREVIOUS_TARGET_DELETION_AND_EXTENSION/has-custom-properties.jsx,',
+                     '/UPDATE_EXTENSION_ON_TARGET_FILE/instanceof.jsx,' ] },
+                 '9':
+                 { description:
+                   [ 'APPEND_MODIFIED_TARGET_WITH_NOTE_ON_SOURCE_DELETION.....' ],
+                   files:
+                   [ '/262-partially-curated-vendor-deleted/date-negative-zero.js',
+                     '/APPEND_MODIFIED_TARGET_WITH_NOTE_ON_SOURCE_DELETION/has-own-property-called-on-non-object.js' ] },
+                 '10':
+                 { description: [ 'RENAME_MODIFIED_TARGET_FILE_WITH_NOTE_ON_RENAME.....' ],
+                   files: [] },
+                 '11':
+                 { description:
+                   [ 'UPDATE_EXTENSION_ON_MODIFIED_TARGET_FILE_WITH_NOTE_ON_EXTENSION_CHANGE.....' ],
+                   files: [] },
+                 '12':
+                 { description: [ 'EXPORT_FILE.....' ],
+                   files:
+                   [ '/262-does-not-exist-vendor-new-exportable-file/for-in-array-mode.js',
+                     '/262-does-not-exist-vendor-new-unexportable-file/es6.yaml' ] },
+                 '13':
+                 { description: [ 'UPDATE_EXTENSION_ON_TARGET_FILE.....' ],
+                   files: [] } }
+
+
+describe('OutcomeReporter', function() {
+  it('should render a file list with the targetSubDirectory', async function() {
+    let reporter = new OutcomeReporter({
+      implConfig: { targetSubDirectory: "test262/implementation-contributed/jsc" }
+    });
+
+    expect(reporter.renderFileList(
+      [
+        '/stress/for-in-array-mode.js',
+        '/stress/for-of-array-mode.js',
+      ]
+    ))
+      .toBe(` - test262/implementation-contributed/jsc/stress/for-in-array-mode.js
+ - test262/implementation-contributed/jsc/stress/for-of-array-mode.js`);
+  });
+
+  it('should use the commit shas in the heading', function() {
+    let reporter = new OutcomeReporter({
+      implConfig: { targetSubDirectory: "test262/implementation-contributed/jsc" }
+    });
+
+    expect(reporter.renderHeading({
+      vendor: 'jsc',
+      sourceSha: 'abc',
+      targetSha: '123',
+    }))
+      .toBe(`
+# Import JavaScript Test Changes from jsc
+
+Changes imported in this pull request include all changes made since
+\`abc\` in jsc and all changes made since \`123\` in
+test262.
+`)
+  });
+
+  it('should not render a subsection if the files array is empty', function() {
+    let reporter = new OutcomeReporter({
+      implConfig: { targetSubDirectory: "test262/implementation-contributed/jsc" }
+    });
+
+    expect(reporter.renderSubSection(0, {files: []}, 'jsc'))
+      .toBe('')
+  });
+
+  it('should render a subsection', function() {
+    let reporter = new OutcomeReporter({
+      implConfig: { targetSubDirectory: "test262/implementation-contributed/jsc" }
+    });
+
+    expect(reporter.renderSubSection(0, {files: ['/a.js']}, 'jsc'))
+      .toBe(`
+### 1 Ignored File
+
+
+These files were updated or added in the jsc repo but they
+are not synced to test262 because they are excluded.
+
+
+ - test262/implementation-contributed/jsc/a.js
+`)
+  });
+
+
+  it('should generate a report', function() {
+    let reporter = new OutcomeReporter({
+      implConfig: { targetSubDirectory: "test262/implementation-contributed/jsc" }
+    });
+
+    expect(reporter.generateReport({
+      sourceSha: '123',
+      targetSha: 'abc',
+      vendor: 'jsc',
+      outcomes: { '0': { files: ['/a.js']}}
+    }))
+      .toBe(`
+
+# Import JavaScript Test Changes from jsc
+
+Changes imported in this pull request include all changes made since
+\`123\` in jsc and all changes made since \`abc\` in
+test262.
+
+
+
+### 1 Ignored File
+
+
+These files were updated or added in the jsc repo but they
+are not synced to test262 because they are excluded.
+
+
+ - test262/implementation-contributed/jsc/a.js
+
+`)
+  });
+});
