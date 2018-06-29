fada5464e46c045fdd85845edd9de39af49f24e7
bmac <bmac325@gmail.com>
2018-06-29 14:30:40 -0400

    use https to push

--- cron.sh
+++ cron.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-node index.js --pull-request --implementation jsc --debug
+node index.js --pull-request --implementation jsc --debug --t262-git-remote "https://$GITHUB_TOKEN@github.com/test262-automation/test262.git"
