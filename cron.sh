08df03771e069aa22a57537163b047702560d573
bmac <bmac325@gmail.com>
2018-06-29 15:20:07 -0400

    use a different approach for keeping travis alive

--- cron.sh
+++ cron.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+while sleep 9m; do echo "=====[ $SECONDS seconds still running ]====="; done &
 node index.js --pull-request --implementation jsc --debug --t262-git-remote "https://$GITHUB_TOKEN@github.com/test262-automation/test262.git"
