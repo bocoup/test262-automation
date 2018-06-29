138301bb2945ee38b019a0633cbb759e0bc97ba3
bmac <bmac325@gmail.com>
2018-06-29 13:46:57 -0400

    Add description for options

--- index.js
+++ index.js
@@ -20,6 +20,46 @@ const argv = yargs
     demandOption: true,
     describe: 'Specify implementor engine...options are jsc',
     type: 'string',
+  })
+  .options({
+    // The following options override the github config
+    't262-github-org': {
+      describe: 'The github org or user that owns the test262 repo',
+    },
+    't262-github-repo-name': {
+      describe: 'The name of the test262 repo on github',
+    },
+    't262-github-base-branch': {
+      describe: 'The branch on test262 to target when opening a pr',
+    },
+    't262-github-username': {
+      describe: 'The user that will open a pull request',
+    },
+    't262-github-remote': {
+      describe: 'The git remote on github to push the branch with changes',
+    },
+    'github-token': {
+      describe: 'A github Oauth token for the user that will be used to open the pull request',
+    },
+    // The following options override implementation configs
+    'target-git': {
+      describe: 'The git repo to use as the target for applying changes',
+    },
+    'target-revision-at-last-export': {
+      describe: 'The starting sha or branch to use when comparing changes since the last sync',
+    },
+    'target-branch': {
+      describe: 'The branch to sync changes too.',
+    },
+    'source-git': {
+      describe: 'The git repo to use as the source of the changes to sync',
+    },
+    'source-revision-at-last-export': {
+      describe: 'The starting sha or branch to use when comparing changes since the last sync',
+    },
+    'source-branch': {
+      describe: 'The branch to sync changes from',
+    }
   }).argv;
 
 
