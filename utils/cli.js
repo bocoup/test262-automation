f714c89cd9f2d5d8c0e06e803b5c332bba454972
Brendan McLoughlin <bmac@users.noreply.github.com>
2018-07-02 11:04:20 -0400

    Update the cli util to allow for overiding the json configs from the command line (#57)

--- utils/cli.js
+++ utils/cli.js
@@ -1,14 +1,11 @@
 const yargs = require('yargs');
-const fs = require('fs')
+const pick = require('lodash.pick');
+const fs = require('fs');
 
 const choices = fs.readdirSync('./config/implementation/')
       .map(file => file.replace(/\.json$/, ''))
       .filter(file => !file.endsWith('-debug'));
 
-function getImplemationConfigOptions() {
-
-  // TODO add config validation method to tie in with CLI
-}
 
 function getConfigOptions() {
 
@@ -25,6 +22,46 @@ function getConfigOptions() {
       describe: 'Specify implementor engine',
       type: 'string',
       choices,
+    })
+    .options({
+      // The following options override the github config
+      't262-github-org': {
+        describe: 'The github org or user that owns the test262 repo',
+      },
+      't262-github-repo-name': {
+        describe: 'The name of the test262 repo on github',
+      },
+      't262-github-base-branch': {
+        describe: 'The branch on test262 to target when opening a pr',
+      },
+      't262-github-username': {
+        describe: 'The user that will open a pull request',
+      },
+      't262-github-remote': {
+        describe: 'The git remote on github to push the branch with changes',
+      },
+      'github-token': {
+        describe: 'A github Oauth token for the user that will be used to open the pull request',
+      },
+      // The following options override implementation configs
+      'target-git': {
+        describe: 'The git repo to use as the target for applying changes',
+      },
+      'target-revision-at-last-export': {
+        describe: 'The starting sha or branch to use when comparing changes since the last sync',
+      },
+      'target-branch': {
+        describe: 'The branch to sync changes too.',
+      },
+      'source-git': {
+        describe: 'The git repo to use as the source of the changes to sync',
+      },
+      'source-revision-at-last-export': {
+        describe: 'The starting sha or branch to use when comparing changes since the last sync',
+      },
+      'source-branch': {
+        describe: 'The branch to sync changes from',
+      }
     }).argv;
 
   let implementationConfig = argv.implementation;
@@ -40,6 +77,16 @@ function getConfigOptions() {
   /* Setup Config */
   implementationConfig = require(`../config/implementation/${implementationConfig}.json`); // TODO add config validation method to tie in with CLI
   githubConfig = require(`../config/${githubConfig}.json`);
+
+  implementationConfig = {
+    ...implementationConfig,
+    ...pick(argv, Object.keys(implementationConfig))
+  };
+
+  githubConfig = {
+    ...githubConfig,
+    ...pick(argv, Object.keys(githubConfig))
+  };
 
   return {
     argv,
