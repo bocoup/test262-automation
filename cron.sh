a42e2816610720c33379d76a18f3cfa0db5e1aa2
Rick Waldron <waldron.rick@gmail.com>
2018-09-12 12:04:10 -0400

    Add v8 to cron.sh

--- cron.sh
+++ cron.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-TMPDIR="./tmp" node index.js --pull-request --implementation jsc $@
+TMPDIR="./tmp-jsc" node index.js --pull-request --implementation jsc $@;
+TMPDIR="./tmp-v8" node index.js --pull-request --implementation v8 $@;
