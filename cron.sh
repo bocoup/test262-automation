638adde1a7ff1fe3b44d3e63ff89ade135eae49f
Rick Waldron <waldron.rick@gmail.com>
2020-02-07 11:33:44 -0500

    Enable V8

--- cron.sh
+++ cron.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 TMPDIR="./tmp-jsc" node index.js --pull-request --implementation jsc $@;
-# TMPDIR="./tmp-v8" node index.js --pull-request --implementation v8 $@;
+TMPDIR="./tmp-v8" node index.js --pull-request --implementation v8 $@;
